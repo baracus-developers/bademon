1557aed37333e22fb8bbf4a6ef29da03bfe9f102
Sven-Thorsten Dietrich <thebigcorporation@gmail.com>
2011-05-26 22:37:35 -0700

    Crude pointer arithmetic for storing query data in 2D grid.

--- baracus-pgsql.c
+++ baracus-pgsql.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <pthread.h>
 #include <syslog.h>
@@ -15,16 +16,17 @@ PGconn * baracus_db_connect(char *hostaddr, char *dbname,
 			    char *username, char *password, 
 			    int port, int timeout) 
 {
-	
 	PGresult *res;
 	char * conninfo;
 	PGconn	 *conn = NULL;
+	int conninfo_len = 128 + strlen(hostaddr) + strlen(dbname) + 
+				 strlen(username) + strlen(password) + 6 + 3;
 
-	conninfo = malloc(128+strlen(hostaddr)+strlen(dbname)+strlen(username)+strlen(password)+6+3);
+	conninfo = malloc(conninfo_len);
 	if (conninfo == NULL) 
 		return NULL;
 	
-		snprintf (conninfo, conninfo_strlen, "hostaddr = '%s' port = '%6d' dbname = '%s' user = '%s' password = '%s' connect_timeout = '%3d'", hostaddr, port, dbname, username, password, timeout);
+	snprintf (conninfo, conninfo_len, "hostaddr = '%s' port = '%6d' dbname = '%s' user = '%s' password = '%s' connect_timeout = '%3d'", hostaddr, port, dbname, username, password, timeout);
 	printf("conninfo is %s\n", conninfo);
 	
 	PQinitOpenSSL(1, 1);
@@ -35,7 +37,6 @@ PGconn * baracus_db_connect(char *hostaddr, char *dbname,
 
 	free (conninfo);
 	return conn;
-	
 }
 
 
@@ -44,6 +45,8 @@ int baracus_query(PGconn *conn, char *query) {
 	int	field_count;
 	int     row;
 	int     col;
+	char **  data;
+	char *x;
 
 	PGresult *res;
 
@@ -67,8 +70,16 @@ int baracus_query(PGconn *conn, char *query) {
         puts("==========================");
 	/* first, print out the attribute names */
 	field_count = PQnfields(res);
-    	for (col = 0; col < field_count; col++)
-        	printf("%-16s", PQfname(res, col));
+        rec_count = PQntuples(res);
+	data = (char **) calloc(field_count * (rec_count+1), sizeof(char *));
+	if (data == NULL) {
+		printf("error allocating data\n");
+		return -1;
+	}
+    	for (col = 0; col < field_count; col++) {
+		data[col] = PQfname(res, col);
+        	printf("%-16s", data[col]);
+	}
     	printf("\n");
     	for (col = 0; col < ((field_count < 5) ? field_count:5); col++)
         	printf("===============");
@@ -78,7 +89,8 @@ int baracus_query(PGconn *conn, char *query) {
 
         for (row=0; row<rec_count; row++) {
                 for (col=0; col < field_count; col++) {
-                        printf("%-16s", PQgetvalue(res, row, col));
+			data[(row+1)*col+col] = PQgetvalue(res, row, col);
+                        printf("%-16s", data[(row+1)*col+col]);
                 }
     		printf("\n");
     		for (col = 0; col < ((field_count < 5) ? field_count:5); col++)
--- baracus-pgsql.h
+++ baracus-pgsql.h
@@ -1,5 +1,10 @@
 #include <pgsql/libpq-fe.h>
 
+typedef struct {
+	int rows;
+	int cols;
+	char **data;
+} baracus_query_rpt;
 
 PGconn * baracus_db_connect(char *hostaddr, char *dbname, char *username, 
 			    char *password, int port, int timeout);
